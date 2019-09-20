3e18ade128ea428329f68f4607d726b90c23423b
Quank123Wip <Quank123Wip@outlook.com>
2019-09-20 22:20:49 +0800

    Updated to invoid XSS

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -10,6 +10,27 @@
 <script lang="ts">
 import Vue from "vue";
 import mavonEditor from "mavon-editor";
+import filterXSS from 'xss'
+filterXSS.whiteList['input'] = ['type', 'src', 'disabled', 'checked', 'class']
+filterXSS.whiteList['img'] = ['src', 'alt', 'title', 'width', 'max-width', 'style', 'height']
+filterXSS.whiteList['math'] = []
+filterXSS.whiteList['semantics'] = []
+filterXSS.whiteList['mrow'] = []
+filterXSS.whiteList['mtext'] = []
+filterXSS.whiteList['annotation'] = ['encoding']
+filterXSS.whiteList['msub'] = []
+filterXSS.whiteList['mi'] = []
+filterXSS.whiteList['mo'] = []
+filterXSS.whiteList['mn'] = []
+filterXSS.whiteList['li'] = ['class']
+filterXSS.whiteList['span'] = ['class', 'title', 'aria-hidden', 'style']
+filterXSS.whiteList['code'] = []
+filterXSS.whiteList['li'] = ['class', 'id']
+filterXSS.whiteList['td'] = ['style', 'class']
+filterXSS.whiteList['p'] = ['style', 'class']
+filterXSS.whiteList['div'] = ['style', 'class']
+filterXSS.whiteList['a'] = ['style', 'class', 'id', 'target', 'href', 'title']
+filterXSS.stripIgnoreTag = true
 import "mavon-editor/dist/css/index.css";
 
 Vue.use(mavonEditor);
@@ -27,7 +48,7 @@ export default Vue.extend({
   },
     methods:{
         Render() {
-            this.html=mavonEditor.markdownIt.render(this.Markdown)
+            this.html=this.filterXSS(mavonEditor.markdownIt.render(this.Markdown))
         }
     }
 });
