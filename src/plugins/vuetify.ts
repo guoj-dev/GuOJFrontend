c352181fcf02d58bcbfb0e069c11576b439c7121
Quank123Wip <Quank123Wip@outlook.com>
2019-10-07 21:21:50 +0800

    Updated Vuetify

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,15 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import zhHans from 'vuetify/src/locale/zh-Hans';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    lang: {
+      locales: { zhHans },
+      current: 'zh-Hans',
+    },
   icons: {
     iconfont: 'mdi',
   },
