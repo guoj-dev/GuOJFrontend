1897bec73b74425824235dfaa5ffdbe0ea88e8a8
Quank123Wip <Quank123Wip@outlook.com>
2020-02-24 12:55:33 +0800

    Added Logout Logic and fixed names

--- src/store/modules/auth.ts
+++ src/store/modules/auth.ts
@@ -1,62 +1,76 @@
 import Axios from 'axios'
 import { Mutation, Action, Getter, State } from 'vuex-simple'
 
-export interface User{
-    Username:String,
-    uid:Number
-}
-
 export interface AuthData{
-    username:String,
-    password:String
+    Username:String,
+    Password:String
 }
 
 export default class AuthModule {
     @State()
-    public status = new String('');
+    public Status = new String('');
 
     @State()
-    public token = new String(localStorage.getItem('token') || '');
+    public Token = new String(localStorage.getItem('token') || '');
 
     @State()
     public user = {};
 
     @Mutation()
     public auth_request(){
-        this.status = 'loading';
+        this.Status = 'loading';
     }
 
     @Mutation()
-    public auth_success(token:String){
-        this.status = 'success';
-        this.token = token;
+    public auth_success(Token:String){
+        this.Status = 'success';
+        this.Token = Token;
     }
 
     @Mutation()
     public auth_error(){
-        this.status = 'error';
+        this.Status = 'error';
     }
 
     @Mutation()
     public auth_logout(){
-        this.status = '';
-        this.token = '';
+        this.Status = '';
+        this.Token = '';
     }
 
     @Action()
-    public login(User:AuthData){
+    public Login(User:AuthData){
         return new Promise((resolve, reject) => {
             this.auth_request();
             Axios.post('http://localhost:8000/auth/login', User).then(Response =>{
-                const token = Response.data.key;
-                localStorage.setItem('token', token);
-                this.auth_success(token);
+                const Token = Response.data.key;
+                localStorage.setItem('token', Token);
+                this.auth_success(Token);
                 resolve(Response);
             }).catch(Error => {
-                this.auth_error(Error);
+                this.auth_error();
                 localStorage.removeItem('token')
                 reject(Error);
             })
         })
+    }
+
+    @Action()
+    public Logout(){
+        return new Promise((resolve, reject) => {
+            this.auth_logout();
+            localStorage.removeItem('token');
+            resolve();
+        })
+    }
+
+    @Getter()
+    public get isLogin(){
+        return !!this.Token;
+    }
+
+    @Getter()
+    public get AuthStatus(){
+        return this.Status;
     }
 }
