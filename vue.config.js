4fc7ba8050750a0a9e32ab91bf4d312e5e542d43
quank123wip <quank123wip@outlook.com>
2020-03-18 22:17:37 +0800

    Add Editor.md

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     configureWebpack: {
         devServer: {
+            historyApiFallback: true,
             proxy: {
                 '/': {
                     target: 'http://localhost:8000',
