e17fdf44eaf96649711d427db926a9e631c006f1
quank123wip <quank123wip@outlook.com>
2020-05-23 09:35:51 +0800

    refactor: Problemset

--- src/views/ProblemSet.vue
+++ src/views/ProblemSet.vue
@@ -8,61 +8,42 @@
             transition="slide-y-reverse-transition"
         >
             <v-card-title>
-                <span>题库</span>
+                <span class="display-1">题库</span>
             </v-card-title>
-            <v-container>
-                <v-row v-for="key in Problems" :key="key">
-                    <v-col>
-                        <v-card
-                            :dark="Theme.Problem.Dark"
-                            :light="!Theme.Problem.Dark"
-                            :color="
-                                Theme.Problem.ProblemColor
-                                    ? key[2]
-                                    : Theme.Problem.Color
-                            "
-                            elevation="3"
-                            :to="'/Problem/' + key[0]"
-                        >
-                            <v-container style="padding: 0 4px 0 0;">
-                                <v-row>
-                                    <v-col>
-                                        <v-card-title style="padding: 0 0 0 0;">
-                                            <v-icon>mdi-chevron-right</v-icon>
-                                            {{ key[0] }} {{ key[1] }}
-                                        </v-card-title>
-                                    </v-col>
-                                    <v-col>
-                                        <div align="right">
-                                            <v-chip
-                                                :dark="Theme.Problem.Tags.Dark"
-                                                :light="!Theme.Problem.Tags.Dark"
-                                                :outlined="Theme.Problem.Tags.Outlined"
-                                                :color="key[3][0]"
-                                                >{{ key[3][1] }}</v-chip
-                                            >
-                                        </div>
-                                    </v-col>
-                                </v-row>
-                            </v-container>
-                        </v-card>
-                    </v-col>
-                </v-row>
-                <div class="text-center">
-                    <v-pagination
-                        v-model="page"
-                        :length="4"
-                        light
-                        circle
-                    ></v-pagination>
-                </div>
-            </v-container>
+            <v-divider />
+            <v-list :color="Theme.Color" shaped="true" subheader="false" tile>
+                <v-list-item v-for="key in Problems" :key="key" link :to="'/Problem/' + key[0]">
+                    <v-list-item-action class="me-2">
+                        <v-icon>mdi-chevron-right</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title class="title align-center">
+                                
+                                {{ key[0] }}
+                                {{ key[1] }}
+                        </v-list-item-title>
+                    </v-list-item-content>
+                    <v-list-item-action>
+                        <v-chip :color="key[3][0]">
+                            {{key[3][1]}}
+                        </v-chip>
+                    </v-list-item-action>
+                </v-list-item>
+            </v-list>
+            <div class="text-center">
+                <v-pagination
+                    v-model="page"
+                    :length="4"
+                    light
+                    circle
+                ></v-pagination>
+            </div>
         </v-card>
     </v-container>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
 
 export default Vue.extend({
     components: {},
@@ -70,77 +51,77 @@ export default Vue.extend({
         return {
             Problems: [
                 [
-                    'P1000',
-                    'A+B Problem',
-                    'light-blue accent-4',
-                    ['green', '入门难度'],
+                    "P1000",
+                    "A+B Problem",
+                    "light-blue accent-4",
+                    ["green", "入门难度"],
                 ],
                 [
-                    'P1001',
-                    'A+B Problem',
-                    'light-blue accent-4',
-                    ['green', '入门难度'],
+                    "P1001",
+                    "A+B Problem",
+                    "light-blue accent-4",
+                    ["green", "入门难度"],
                 ],
                 [
-                    'P1002',
-                    'A+B Problem',
-                    'light-blue accent-4',
-                    ['green', '入门难度'],
+                    "P1002",
+                    "A+B Problem",
+                    "light-blue accent-4",
+                    ["green", "入门难度"],
                 ],
                 [
-                    'P1003',
-                    'A+B Problem',
-                    'light-blue accent-4',
-                    ['green', '入门难度'],
+                    "P1003",
+                    "A+B Problem",
+                    "light-blue accent-4",
+                    ["green", "入门难度"],
                 ],
                 [
-                    'P1005',
-                    'A+B Problem',
-                    'light-blue accent-4',
-                    ['green', '入门难度'],
+                    "P1005",
+                    "A+B Problem",
+                    "light-blue accent-4",
+                    ["green", "入门难度"],
                 ],
                 [
-                    'P1006',
-                    'A+B Problem',
-                    'light-blue accent-4',
-                    ['green', '入门难度'],
+                    "P1006",
+                    "A+B Problem",
+                    "light-blue accent-4",
+                    ["green", "入门难度"],
                 ],
                 [
-                    'P1007',
-                    'A+B Problem',
-                    'light-blue accent-4',
-                    ['green', '入门难度'],
+                    "P1007",
+                    "A+B Problem",
+                    "light-blue accent-4",
+                    ["green", "入门难度"],
                 ],
                 [
-                    'P1008',
-                    'A+B Problem',
-                    'light-blue accent-4',
-                    ['green', '入门难度'],
+                    "P1008",
+                    "A+B Problem",
+                    "light-blue accent-4",
+                    ["green", "入门难度"],
                 ],
                 [
-                    'P1009',
-                    'A+B Problem',
-                    'light-blue accent-4',
-                    ['green', '入门难度'],
+                    "P1009",
+                    "A+B Problem",
+                    "light-blue accent-4",
+                    ["green", "入门难度"],
                 ],
             ],
             items: [
                 {
-                    text: 'GuOJ',
+                    text: "GuOJ",
                     disabled: true,
-                    href: '/',
+                    href: "/",
                 },
                 {
-                    text: 'ProblemSet',
+                    text: "ProblemSet",
                     disabled: true,
-                    href: '/ProblemSet',
+                    href: "/ProblemSet",
                 },
             ],
         };
     },
     computed: {
         Theme() {
-            return this.$store.getters['Theme/ProblemSet'];
+            return this.$store.getters["Theme/ProblemSet"];
         },
     },
 });
