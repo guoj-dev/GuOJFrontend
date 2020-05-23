0361f8001218c5fd24938aa3a3107960f64c0490
quank123wip <quank123wip@outlook.com>
2020-05-23 09:36:02 +0800

    lint: MaterialCard

--- src/components/MaterialCard.vue
+++ src/components/MaterialCard.vue
@@ -1,57 +1,70 @@
 <template>
-  <v-card v-bind="$attrs" :class="classes" class="v-card--material pa-3" :color="color">
-    <div class="d-flex grow flex-wrap">
-      <v-avatar
-        v-if="avatar"
-        size="128"
-        class="mx-auto v-card--material__avatar elevation-6"
-        color="grey"
-      >
-        <v-img :src="avatar" />
-      </v-avatar>
+    <v-card
+        v-bind="$attrs"
+        :class="classes"
+        class="v-card--material pa-3"
+        :color="color"
+    >
+        <div class="d-flex grow flex-wrap">
+            <v-avatar
+                v-if="avatar"
+                size="128"
+                class="mx-auto v-card--material__avatar elevation-6"
+                color="grey"
+            >
+                <v-img :src="avatar" />
+            </v-avatar>
 
-      <v-sheet
-        v-else
-        :class="{
-          'pa-7': !$slots.image
-        }"
-        :color="sheetcolor"
-        :max-height="icon ? 90 : undefined"
-        :width="icon ? 'auto' : '100%'"
-        elevation="6"
-        class="text-start v-card--material__heading mb-n6"
-        dark
-      >
-        <slot v-if="$slots.heading" name="heading" />
+            <v-sheet
+                v-else
+                :class="{
+                    'pa-7': !$slots.image,
+                }"
+                :color="sheetcolor"
+                :max-height="icon ? 90 : undefined"
+                :width="icon ? 'auto' : '100%'"
+                elevation="6"
+                class="text-start v-card--material__heading mb-n6"
+                dark
+            >
+                <slot v-if="$slots.heading" name="heading" />
 
-        <slot v-else-if="$slots.image" name="image" />
+                <slot v-else-if="$slots.image" name="image" />
 
-        <div v-else-if="title && !icon" class="display-1 font-weight-light" v-text="title" />
+                <div
+                    v-else-if="title && !icon"
+                    class="display-1 font-weight-light"
+                    v-text="title"
+                />
 
-        <v-icon v-else-if="icon" size="32" v-text="icon" />
+                <v-icon v-else-if="icon" size="32" v-text="icon" />
 
-        <div v-if="text" class="headline font-weight-thin" v-text="text" />
-      </v-sheet>
+                <div
+                    v-if="text"
+                    class="headline font-weight-thin"
+                    v-text="text"
+                />
+            </v-sheet>
 
-      <div v-if="$slots['after-heading']" class="ml-6">
-        <slot name="after-heading" />
-      </div>
+            <div v-if="$slots['after-heading']" class="ml-6">
+                <slot name="after-heading" />
+            </div>
 
-      <div v-else-if="icon && title" class="ml-4">
-        <div class="card-title font-weight-light" v-text="title" />
-      </div>
-    </div>
+            <div v-else-if="icon && title" class="ml-4">
+                <div class="card-title font-weight-light" v-text="title" />
+            </div>
+        </div>
 
-    <slot />
+        <slot />
 
-    <template v-if="$slots.actions">
-      <v-divider class="mt-2" />
+        <template v-if="$slots.actions">
+            <v-divider class="mt-2" />
 
-      <v-card-actions class="pb-0">
-        <slot name="actions" />
-      </v-card-actions>
-    </template>
-  </v-card>
+            <v-card-actions class="pb-0">
+                <slot name="actions" />
+            </v-card-actions>
+        </template>
+    </v-card>
 </template>
 
 <script lang="ts">
@@ -61,37 +74,37 @@ export default {
     props: {
         avatar: {
             type: String,
-            default: ''
+            default: '',
         },
         color: {
             type: String,
-            default: 'primary'
+            default: 'primary',
         },
         sheetcolor: {
             type: String,
-            default: 'success'
+            default: 'success',
         },
         icon: {
             type: String,
-            default: undefined
+            default: undefined,
         },
         image: {
             type: Boolean,
-            default: false
+            default: false,
         },
         text: {
             type: String,
-            default: ''
+            default: '',
         },
         title: {
             type: String,
-            default: ''
-        }
+            default: '',
+        },
     },
     computed: {
         classes() {
             return {
-                'v-card--material--has-heading': this.hasHeading
+                'v-card--material--has-heading': this.hasHeading,
             };
         },
         hasHeading() {
@@ -99,8 +112,8 @@ export default {
         },
         hasAltHeading() {
             return Boolean(this.$slots.heading || (this.title && this.icon));
-        }
-    }
+        },
+    },
 };
 </script>
 
